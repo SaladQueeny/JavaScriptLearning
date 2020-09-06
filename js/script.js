91cc63da0fbdcd42d7ebcee50aa731649f2cd0dc
EgorPC <kolpakov.egor@icloud.com>
2020-09-06 18:54:48 +0300

    2 practice

--- js/script.js
+++ js/script.js
@@ -1,4 +1,16 @@
 "use srtict";
 
+function showfirstMessage(text){//создаётся сразу
+    console.log(text);
+    
+}
 
+showfirstMessage("hi");
 
+const logger = function() {//создаётся по ходу кода
+    console.log("hello");
+};
+
+logger();
+
+const calc = (a,b)=>{ return a+b};//Стрелочная функция
