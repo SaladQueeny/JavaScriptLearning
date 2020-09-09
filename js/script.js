8cd242bc63a23f6d43d3d8ef30cff90035688d29
EgorPC <kolpakov.egor@icloud.com>
2020-09-09 22:02:54 +0300

    lesson 22

--- js/script.js
+++ js/script.js
@@ -1,42 +1,7 @@
 "use srtict";
-//объекты
+//передача по ссылке или по значению
+let a=5, b=a;
+b+=5;
 
-const options = {
-    name: 'test',
-    width: 1024,
-    height: 1024,
-    colors: { //объект внутри объекта
-        border: 'black',
-        background: 'red'
-    },
-    makeTest: function(){//метод объекта
-        console.log("Test");
-    }
-};
-options.makeTest();
-// delete options.name;
-
-const {border, background} = options.colors;//деструктуризация
-console.log(border);//black
-
-
-
-
-//перебор сложный и ненужный
-// let counter =0;
-// for (let key in options) { //перебор
-//     if (typeof (options[key]) == 'object') {
-//         for (let i in options[key]) {
-//             console.log(`Свойство ${i} имеет значение ${options[key][i]}`);
-//             counter++;
-//         }
-//     } else {
-//         console.log(`Свойство ${key} имеет значение ${options[key]}`);
-//         counter++;
-//     }
-// }
-// console.log(counter);
-
-
-//перебор простой
-console.log(Object.keys(options).length);
+console.log(b);
+console.log(a);
