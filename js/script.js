93385c382e9f1ac1f8a5e4502de48bcb2be412b7
EgorPC <kolpakov.egor@icloud.com>
2020-09-22 21:40:20 +0300

    lesson 29

--- js/script.js
+++ js/script.js
@@ -1,26 +1,53 @@
 "use srtict";
-//получение элементов со страницы
-const box = document.getElementById("box");
+//действия с элементами на странице
+const box = document.getElementById("box"),
+      btns = document.getElementsByTagName("button"),
+      circles = document.getElementsByClassName("circle"),
+      hearts = document.querySelectorAll(".heart"),
+      oneHeart = document.querySelector(".heart"),
+      wrapper = document.querySelector(".wrapper");
 
-console.log(box);
+//console.dir(box);//увидим все свойства элемента
 
-const btns = document.getElementsByTagName("button");//псевдомассив
-const btns1 = document.getElementsByTagName("button")[1];//только вторая кнопка
+// box.style.backgroundColor = "blue";
+// box.style.width = "500px";
+const num =400;
+box.style.cssText = `background-color: blue; width: ${num}px`;
+btns[1].style.borderRadius = "100%";
+circles[0].style.backgroundColor = "red";
 
-const circels = document.getElementsByClassName("circles");//html коллекция
-console.log(circels);
+// for(let i =0 ; i < hearts.length; i++){
+//     hearts[i].style.backgroundColor = "blue";
+// }
+ hearts.forEach(item => {
+    item.style.backgroundColor = "blue";
+ });
 
-const hearts = document.querySelectorAll(".heart");//во внуть помещаем css селектор
+ const div = document.createElement("div");//только внутри скрипта
 
-console.log(hearts);
+ //const text = document.createTextNode("Тут был я");
 
-hearts.forEach(item=>{
-    console.log(item);
-});
+div.classList.add("black");
+document.body.append(div);//добавляет в конец body
 
-const oneHeart = document.querySelector("div");//только один элемент с остраницы(первый с селектора)
-console.log(oneHeart);//получим первую коробку
+//.append(div);
+//wrapper.appendChild(div);
 
+//wrapper.prepend(div);//вставляет в начало
 
+//hearts[0].before(div);
+//hearts[0].after(div);
 
+//wrapper.insertBefore(div, hearts[0]);
 
+//circles[0].remove();
+//wrapper.removeChild(hearts[1]);
+
+//hearts[0].replaceWith(circles[0]);//заменяет
+//wrapper.replaceChild(circles[0], hearts[0]);
+
+div.innerHTML = "<h1>Hello World</h1>";
+
+//div.textContent="hello";//Только текст 
+
+div.insertAdjacentHTML("beforebegin","<h2>Hello</h2>");//первый аргумент определяет положение
